f7b2c4996d9fb982f82ef5211a94560e43fbec77
Tyler Webster <twebster137@gmail.com>
2020-02-16 15:48:37 -0600

    Delete rust-mode and pdf-tools.

--- init.el
+++ init.el
@@ -6,15 +6,12 @@
 ;; temporary https "fix" for gnu packages (instead of using http)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
-;; package list
-(setq installed-packages '(rainbow-delimiters
+(setq package-list '(rainbow-delimiters
 			   multiple-cursors
 			   evil
 			   base16-theme
-			   rust-mode
 			   lsp-mode
 			   paredit
-			   pdf-tools
 			  ))
 
 (custom-set-variables
@@ -24,7 +21,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (pdf-tools evil paredit lsp-mode rust-mode multiple-cursors rainbow-delimiters base16-theme))))
+    (evil paredit lsp-mode multiple-cursors rainbow-delimiters base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
