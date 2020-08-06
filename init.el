982268d7913345d233948208a0441d12f5b73fa6
Tyler Webster <twebster137@gmail.com>
2020-08-05 20:23:57 -0500

    Remove uninstalled packages, add config reload function and some  lisp mode hooks.

--- init.el
+++ init.el
@@ -6,24 +6,12 @@
 ;; temporary https "fix" for gnu packages (instead of using http)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
-(setq package-list '(rainbow-delimiters
-			   multiple-cursors
-			   evil
-			   evil-magit
-			   magit
-			   base16-theme
-			   lsp-mode
-			   paredit
-			  ))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (evil-magit magit evil paredit lsp-mode multiple-cursors rainbow-delimiters base16-theme))))
+ '(package-selected-packages (quote (dracula-theme evil paredit rainbow-delimiters))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
