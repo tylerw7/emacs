a4a66b64b402ace6d06f5a2e6985a86a8c58bdda
Tyler Webster <twebster137@gmail.com>
2020-08-12 19:19:16 -0500

    Add company mode and geiser.

--- init.el
+++ init.el
@@ -11,7 +11,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (dracula-theme evil paredit rainbow-delimiters))))
+ '(package-selected-packages
+   (quote
+    (company geiser dracula-theme evil paredit rainbow-delimiters))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
